b01edd2aec709e86d6189dc14cba33fa4485a13c
robernol <robernol@OA-S141-24>
2026-01-14 17:31:07 -0500

    Week 2 Class

---
+++ 23350-NodeCanvas-main/Assets/Scripts/ApproachAT.cs
@@ -0,0 +1,56 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class ApproachAT : ActionTask {
+
+		public BBParameter<float> speed;
+		public Transform target;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			//Blackboard agentBlackboard = agent.GetComponent<Blackboard>();
+			//speed = agentBlackboard.GetVariableValue<float>("speed");
+
+            //agentBlackboard.SetVariableValue("speed", 0f);
+
+
+            return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			//move towards target transform
+			Vector3 directionToMove = target.position - agent.transform.position;
+			agent.transform.position += directionToMove.normalized * speed.value * Time.deltaTime;
+
+			float distanceToTarget = directionToMove.magnitude;
+			if(distanceToTarget < 0.5f)
+			{
+				EndAction(true);
+
+			}
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
---
+++ 23350-NodeCanvas-main/Assets/Scripts/OverValueCT.cs
@@ -0,0 +1,39 @@
+using NodeCanvas.Framework;
+
+namespace NodeCanvas.Tasks.Conditions
+{
+
+    public class OverValueCT : ConditionTask
+    {
+
+        public BBParameter<float> currentValue;
+        public float threshold;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit()
+        {
+            return null;
+        }
+
+        //Called whenever the condition gets enabled.
+        protected override void OnEnable()
+        {
+
+        }
+
+        //Called whenever the condition gets disabled.
+        protected override void OnDisable()
+        {
+
+        }
+
+        //Called once per frame while the condition is active.
+        //Return whether the condition is success or failure.
+        protected override bool OnCheck()
+        {
+            bool isOverThreshold = currentValue.value > threshold;
+            return isOverThreshold;
+        }
+    }
+}
